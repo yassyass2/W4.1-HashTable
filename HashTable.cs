f59d9e46b2bc39b374c6569e4777307cb3e62100
yassyass2 <yassinabde@outlook.com>
2025-03-19 11:41:01 +0100

    Setup files

---
+++ HashTable.cs
@@ -0,0 +1,50 @@
+using System.Collections.ObjectModel;
+
+namespace Solution;
+
+public class HashTable<K, V> : IHashTable<K, V>
+{
+    Entry<K, V>[]? buckets { get; set;}
+
+    public ReadOnlyCollection<Entry<K, V>> data => buckets == null? null : buckets.AsReadOnly();
+
+    public HashTable() { buckets = null; }
+
+    public HashTable(Entry<K, V>[]? input) { importData(input);}
+
+    public HashTable(int capacity)
+    {
+        buckets = new Entry<K, V>[capacity];
+    }
+
+    protected int getIndex(K key)
+    {
+        int hashCode = Math.Abs(key.GetHashCode());
+        throw new NotImplementedException();
+    }
+
+    public bool Add(K key, V value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public V? Find(K key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(K key)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    //DO NOT REMOVE the following method:
+    private void importData(Entry<K, V>[]? inputData){
+        if(inputData != null) {
+            buckets = new Entry<K, V>[inputData.Length];
+            for (int i = 0; i < inputData.Length; ++i) 
+                buckets[i] = inputData[i];
+        }
+    }
+}
